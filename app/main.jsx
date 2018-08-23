c378cfea769674a2f994f3debc351833dcd72aec
Peter Orum <peter@peterorum.com>
2018-08-23 10:28:30 +1000

    update eslint

--- app/main.jsx
+++ app/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Provider from 'react-redux/lib/components/Provider';
-import store, { history } from '@constants/store';
+import store, { history } from '@constants/store'; // eslint-disable-line
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import persistStore from 'redux-persist/lib/persistStore';
 import { ConnectedRouter } from 'react-router-redux';
-import App from '@containers/app/index';
+import App from '@containers/app/index'; // eslint-disable-line
 
 const persistor = persistStore(store);
 
